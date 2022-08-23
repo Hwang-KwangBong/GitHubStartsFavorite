ed1a0c07b660dd0b42eaec0100b0280d5f57cb13
Kwangbong Hwang <capbong@medibloc.org>
2022-08-24 03:07:59 +0900

    불필요한 소스 제거

--- GitHubStarsFavorite/Classes/Manager/DataManager.swift
+++ GitHubStarsFavorite/Classes/Manager/DataManager.swift
@@ -16,27 +16,6 @@ class DataManager {
     
     let viewModelFavoriteLocal:GithubStarsFavoriteLocalViewModel = GithubStarsFavoriteLocalViewModel()
     
-//    var userData = [User]()
-//    var originLocalUsers = [LocalUser]()
-//    var localUsers: Observable<[LocalUser]>
-//    var filteringLocalUsers = [LocalUser]()
-//
-//    let modelGithubStarsFavoriteAPI:PublishSubject<[User]> = PublishSubject()
-//
-//    public let error:PublishSubject<commonError> = PublishSubject()
-//
-//    init() {
-//        let ordering = BehaviorRelay<Ordering>(value: .byName)
-//        localUsers = ordering
-//            .distinctUntilChanged()
-//            .flatMapLatest { ordering -> Observable<[LocalUser]> in
-//                switch ordering {
-//                case .byName:
-//                    return Current.localUsers().localUsersOrderedByName()
-//                }
-//            }
-//    }
-//
     func setIsFavorite(localUser : LocalUser) {
         self.viewModelFavoriteAPI.userData.indices.filter{self.viewModelFavoriteAPI.userData[$0].id == localUser.id}.forEach{self.viewModelFavoriteAPI.userData[$0].isFavorite = localUser.isFavorite}
         self.viewModelFavoriteAPI.modelGithubStarsFavoriteAPI.onNext(viewModelFavoriteAPI.userData)
